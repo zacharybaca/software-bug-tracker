766a91c75996b27697d18502038dcc08c6cda469
zacharybaca <zachary.baca@msn.com>
2024-09-02 13:46:51 -0500

    added buttons and header

--- client/software-bug-tracker/src/App.jsx
+++ client/software-bug-tracker/src/App.jsx
@@ -3,7 +3,7 @@ import TaskList from './components/TaskList/TaskList';
 import EmployeeForm from './components/EmployeeForm/EmployeeForm';
 import { TasksContextProvider } from './context/tasksContext';
 import { EmployeesContextProvider } from './context/employeesContext';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -11,6 +11,20 @@ function App() {
 
   return (
     <div id="app-container">
+      <h1 id="application-title-heading">Issue Insight</h1>
+      <div id="action-buttons">
+        <Link to="/add-task">
+          <button type="button" id="add-task-main-button">
+            Add Task
+          </button>
+        </Link>
+        <Link to="/add-employee">
+          <button type="button" id="add-employee-main-button">
+            Add Employee
+          </button>
+        </Link>
+      </div>
+
       <Routes>
         <Route
           path="/"
