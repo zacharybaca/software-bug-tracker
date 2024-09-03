5501fcbef30e4a7b36e09c49f65ccee01d2f05d9
zacharybaca <zachary.baca@msn.com>
2024-09-03 06:53:13 -0500

    connected tasklist to context state

--- client/software-bug-tracker/src/components/TaskList/TaskList.jsx
+++ client/software-bug-tracker/src/components/TaskList/TaskList.jsx
@@ -1,12 +1,18 @@
 import './task-list.css';
 import Task from '../Task/Task';
-
+import React from 'react';
+import { TasksContext } from '../../context/tasksContext';
 
 
 const TaskList = () => {
+  const context = React.useContext(TasksContext);
+
+
     return (
       <ul id="task-list">
-        <Task />
+        {context.tasks.map(task => (
+          <Task key={task._id} title={task.taskTitle} completed={task.taskCompleted} details={task.taskDetails} todos={task.taskTodos} assigned={task.assignedEmployee}/>
+        ))}
       </ul>
     );
 }
