5798ce74a9dc0c42a27c6647777d285a657a1fe2
Zachary Baca <zachary.baca@msn.com>
2024-09-06 14:11:22 -0500

    Update SignUpForm.jsx

--- client/software-bug-tracker/src/components/SignUpForm/SignUpForm.jsx
+++ client/software-bug-tracker/src/components/SignUpForm/SignUpForm.jsx
@@ -1,15 +1,15 @@
 import './sign-up-form.css';
 import React from 'react';
-import { EmployeesContext } from '../../context/employeesContext';
+
 
 
 function SignUpForm() {
-  const context = React.useContext(EmployeesContext);
 
     const [user, setUser] = React.useState({
       user: {
         userID: "",
-        password: ""
+        password: "",
+        accessCode: ""
       }
     });
 
@@ -36,10 +36,11 @@ function SignUpForm() {
      function handleSubmit(e) {
        e.preventDefault();
        props.submitEmployee(employee, employee._id);
-       setEmployee({
+       setUser({
         user: {
           userID: "",
-          password: ""
+          password: "",
+          accessCode: ""
         }
        });
      }
@@ -49,10 +50,11 @@ function SignUpForm() {
         <h1 id="sign-up-heading">Sign Up For Account Access</h1>
         <form id="sign-up-form" name="signUpForm" onSubmit={handleSubmit}>
             <label htmlFor="userID">Create A Username: </label>
-            <input type="text" id="user-name" name="userID" value={employee.user.userID} onChange={handleChange} placeholder="Username" />
+            <input type="text" id="user-name" name="userID" value={user.userID} onChange={handleChange} placeholder="Username" />
             <label htmlFor="password">Create A Password: </label>
-            <input type="password" id="password" name="password" value={employee.user.password} onChange={handleChange} placeholder="Password" />
-            
+            <input type="password" id="password" name="password" value={user.password} onChange={handleChange} placeholder="Password" />
+            <label htmlFor="accessCode">Please Enter Access Code To Create An Account: </label>
+            <input type="text" id="accessCode" name="accessCode" value={user.accessCode} onChange={handleChange}/>
             <button type="submit" id="sign-up-form-button">Sign Up!</button>
         </form>
         <button type="button" id="existing-user-button">Already A User? Click Here to Login</button>
