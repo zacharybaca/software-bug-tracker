16c1e15c3c2afeab18a44e24526a915c43d526af
zacharybaca <zachary.baca@msn.com>
2024-09-10 15:47:24 -0500

    added employee directory component

---
+++ client/software-bug-tracker/src/components/EmployeeDirectory/EmployeeDirectory.jsx
@@ -0,0 +1,12 @@
+import './employee-directory.css';
+
+
+const EmployeeDirectory = () => {
+    return (
+        <ul id="employee-directory-list">
+            <li>Employee</li>
+        </ul>
+    )
+}
+
+export default EmployeeDirectory;
