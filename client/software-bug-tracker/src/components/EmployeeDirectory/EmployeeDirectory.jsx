9da98ec4d0e1fca1bd84679b0122a89c42b20749
zacharybaca <zachary.baca@msn.com>
2024-09-11 02:09:06 -0500

    successfully displayed employee data in employee directory component

--- client/software-bug-tracker/src/components/EmployeeDirectory/EmployeeDirectory.jsx
+++ client/software-bug-tracker/src/components/EmployeeDirectory/EmployeeDirectory.jsx
@@ -1,11 +1,25 @@
 import './employee-directory.css';
-
+import { EmployeesContext } from '../../context/employeesContext';
+import React from 'react';
 
 const EmployeeDirectory = () => {
+  const context = React.useContext(EmployeesContext);
+
+
     return (
       <div id="employee-directory-container">
         <ul id="employee-directory-list">
-          <li className="employee">Employee</li>
+          {context.employees.map((employee) => (
+            <li key={employee._id} className="employee">
+              <h1>First Name: {employee.firstName}</h1>
+              <h1>Last Name: {employee.lastName}</h1>
+              <h3>
+                Position:{" "}
+                {employee.roleAtCompany.charAt(0).toUpperCase() +
+                  employee.roleAtCompany.slice(1)}
+              </h3>
+            </li>
+          ))}
         </ul>
       </div>
     );
