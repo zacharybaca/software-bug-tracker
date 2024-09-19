8916a6b9da327d4514d6c39b9ddb1c9ec58ec7c8
Zachary Baca <zachary.baca@msn.com>
2024-09-19 08:17:57 -0500

    Update main.jsx

--- client/software-bug-tracker/src/main.jsx
+++ client/software-bug-tracker/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
       <TasksContextProvider>
         <Router>
           <App />
-      </Router>
+        </Router>
       </TasksContextProvider>
     </EmployeesContextProvider>
     
