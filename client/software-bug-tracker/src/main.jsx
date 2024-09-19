501b5367a7e7b77a11a8edc4e6f6e82ef4f1ce89
Zachary Baca <zachary.baca@msn.com>
2024-09-19 08:13:48 -0500

    Update main.jsx

--- client/software-bug-tracker/src/main.jsx
+++ client/software-bug-tracker/src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { EmployeesContextProvider } from './context/employeesContext.jsx';
+import { TasksContextProvider } from './context/tasksContext.jsx';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <EmployeesContextProvider>
+      <TasksContextProvider>
+        <Router>
+          <App />
+      </Router>
+      </TasksContextProvider>
+    </EmployeesContextProvider>
+    
   </StrictMode>
 );
