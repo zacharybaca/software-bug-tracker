42398875607bdfec5ca70eacd3024bd7e39f3772
Zachary Baca <zachary.baca@msn.com>
2024-09-19 11:20:59 -0500

    Update Footer.jsx

--- client/software-bug-tracker/src/components/Footer/Footer.jsx
+++ client/software-bug-tracker/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import './footer.css';
+import Logo from '../../assets/issue-insight-logo.png';
 
 function Footer() {
     return (
         <footer id="footer">
             <p>Icons Provided By: <a href="https://icons8.com">Icons8</a></p>
-            <p>Issue Insight ⚙️</p>
+            <img src={Logo} alt="logo" />
             <p>©️2024</p>
         </footer>
     )
