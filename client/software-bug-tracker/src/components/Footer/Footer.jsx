2fd35984456d24f87a1370e87c1999e9c0fa5225
Zachary Baca <zachary.baca@msn.com>
2024-09-19 10:37:17 -0500

    Update Footer.jsx

--- client/software-bug-tracker/src/components/Footer/Footer.jsx
+++ client/software-bug-tracker/src/components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import './footer.css';
 function Footer() {
     return (
         <footer id="footer">
-            <a href="https://icons8.com">Icons8</a>
+            <p>Icons Provided By: <a href="https://icons8.com">Icons8</a></p>
+            <p>Issue Insight ⚙️</p>
+            <p>©️2024</p>
         </footer>
     )
 }
