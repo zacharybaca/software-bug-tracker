ef72e112a77a102c5d9b61b777bb963f14a1af9a
zacharybaca <zachary.baca@msn.com>
2024-10-14 12:11:31 -0500

    added basic boilerplate code for web sockets

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 require("dotenv").config();
+const server = require('http').createServer(app);
+const webSocket = require('ws');
 const mongoose = require('mongoose');
 const {expressjwt} = require('express-jwt');
 const app = express();
 const path = require('path');
 
+const wss = new webSocket.Server({ server:server });
+
+wss.on('connection', (ws) => {
+    console.log('A new client Connected!');
+    ws.send('Welcome!');
+    ws.on('message', (message) => {
+        console.log('Received: %s', message);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === webSocket.OPEN) {
+                client.send(message);
+            }
+        })
+    });
+});
 
 // Middleware For Reading Requests From Body
 app.use(express.json());
