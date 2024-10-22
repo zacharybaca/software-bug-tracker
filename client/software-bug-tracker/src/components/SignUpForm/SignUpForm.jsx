e501076fe5e6dea656e51cf24d40cca703d9cb94
zacharybaca <zachary.baca@msn.com>
2024-10-21 19:22:56 -0500

    fixed some UI bugs

--- client/software-bug-tracker/src/components/SignUpForm/SignUpForm.jsx
+++ client/software-bug-tracker/src/components/SignUpForm/SignUpForm.jsx
@@ -31,8 +31,14 @@ function SignUpForm() {
             isAdmin: true,
           }));
         }
+        else if (name === "roleAtCompany") {
+          setEmployee((prevState) => ({
+            ...prevState,
+            isAdmin: false
+          }));
+        }
 
-        if (name === "userID" || name === "password") {
+        if (name === "userID" || name === "password" || name === "associatedEmployee") {
           setEmployee((prevState) => ({
             ...prevState,
             user: {
@@ -100,16 +106,16 @@ function SignUpForm() {
         <h1 id="sign-up-heading">Sign Up For Account Access</h1>
         <form id="sign-up-form" name="signUpForm" onSubmit={handleSubmit}>
             <label htmlFor="userID">Create A Username: </label>
-            <input type="text" id="userID" name="userID" value={employee.user.userID} onChange={handleChange} required placeholder="Username" />
+            <input type="text" id="userID" name="userID" value={employee.user.userID} onChange={handleChange} aria-required="true" placeholder="Username" />
             <label htmlFor="password">Create A Password: </label>
-            <input type="password" id="password" name="password" value={employee.user.password} onChange={handleChange} required placeholder="Password" />
+            <input type="password" id="password" name="password" value={employee.user.password} onChange={handleChange} aria-required="true" placeholder="Password" />
             <label htmlFor="associatedEmployee">Select Associated Employee: </label>
             <select
               id="associatedEmployee"
               name="associatedEmployee"
               value={employee.user.associatedEmployee}
               onChange={handleChange}
-              required
+              aria-required="true"
             >
               <option value="">Select Associated Employee</option>
               {context.employees.map((employee) => (
@@ -122,7 +128,7 @@ function SignUpForm() {
               ))}
             </select>
             <label htmlFor="accessCode">Please Enter Access Code To Create An Account: </label>
-            <input type="text" id="accessCode" name="accessCode" value={employee.accessCode} onChange={handleChange} required placeholder="Access Code"/>
+            <input type="text" id="accessCode" name="accessCode" value={employee.accessCode} onChange={handleChange} aria-required="true" placeholder="Access Code"/>
             <button type="submit" id="sign-up-form-button">Sign Up!</button>
         </form>
         <Link to='/'><button type="button" id="existing-user-button">Already A User? Click Here to Login</button></Link>
