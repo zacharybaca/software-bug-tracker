f43975c1cbcaa38294e83ca5949a909825906442
Zachary Baca <zachary.baca@msn.com>
2024-11-08 11:57:37 -0600

    Update EmployeeDirectory.jsx

--- client/software-bug-tracker/src/components/EmployeeDirectory/EmployeeDirectory.jsx
+++ client/software-bug-tracker/src/components/EmployeeDirectory/EmployeeDirectory.jsx
@@ -5,14 +5,14 @@ import EmployeeBadge from "../EmployeeBadge/EmployeeBadge";
 import React from "react";
 
 const EmployeeDirectory = () => {
-  const context = React.useContext(EmployeesContext);
-  const taskContext = React.useContext(TasksContext);
+  const employeesContext = React.useContext(EmployeesContext);
+  const tasksContext = React.useContext(TasksContext);
 
   return (
     <div id="employee-directory-container">
       <h2 id="employee-directory-heading">Employee Directory</h2>
       <ul id="employee-directory-list">
-        {context.employees.map((employee, index) => (
+        {employeesContext?.employees?.map((employee, index) => (
           <li key={employee._id} className="employee">
             <EmployeeBadge
               itemNumber={index}
@@ -20,17 +20,19 @@ const EmployeeDirectory = () => {
               firstName={employee.firstName}
               lastName={employee.lastName}
               roleAtCompany={employee.roleAtCompany}
-              userID={employee.user.userID}
-              password={employee.user.password}
+              userID={employee.user?.userID}
+              password={employee.user?.password}
               associatedEmployee={employee._id}
               isAdmin={employee.isAdmin}
               accessCode={employee.accessCode}
               avatar={employee.avatar}
               avatarUrl={employee.avatar}
-              updateEmployeeProfile={context.updateEmployee}
-              deleteEmployee={context.deleteEmployee}
-              unAssignTasksForDeletedEmployee={taskContext.unAssignTasksForDeletedEmployee}
-              errMsg={context.userState.errMsg}
+              updateEmployeeProfile={employeesContext.updateEmployee}
+              deleteEmployee={employeesContext.deleteEmployee}
+              unAssignTasksForDeletedEmployee={
+                tasksContext?.unAssignTasksForDeletedEmployee
+              }
+              errMsg={employeesContext.userState?.errMsg}
             />
           </li>
         ))}
