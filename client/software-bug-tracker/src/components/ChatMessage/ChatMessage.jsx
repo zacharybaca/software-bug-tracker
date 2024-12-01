9a8b67600a70a8c265a3b28af43044497e9f5c14
zacharybaca <zachary.baca@msn.com>
2024-12-01 17:38:43 -0600

    made some changes to chat message component

--- client/software-bug-tracker/src/components/ChatMessage/ChatMessage.jsx
+++ client/software-bug-tracker/src/components/ChatMessage/ChatMessage.jsx
@@ -9,7 +9,6 @@ function ChatMessage(props) {
       messageIndex: props.messageIndex ? props.messageIndex : "",
       text: props.text ? props.text : "",
       font: props.font ? props.font : "",
-      user: props.user ? props.user : null,
       date: props.date ? props.date : "",
       loggedInEmployee: props.loggedInEmployee ? props.loggedInEmployee : null,
       avatar: props.loggedInEmployee && props.loggedInEmployee.avatar
@@ -18,15 +17,37 @@ function ChatMessage(props) {
     };
 
     return (
-        <div className={chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0 ? "container darker" : "container"}>
-          <img
-            src={chatMessage.avatar}
-            alt="Avatar"
-            style="width:100%;"
-          />
-          <p className={chatMessage.font}>{chatMessage.text}</p>
-          <span className={chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0 ? "time-left" : "time-right"}>{moment(chatMessage.date).format("h:mm a")}</span>
-        </div>
+      <div
+        className={
+          chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0
+            ? "container darker"
+            : "container"
+        }>
+        <img
+          src={chatMessage.avatar}
+          alt="Avatar"
+          style="width:100%;"
+          className={
+            chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0 ? "right" : ""
+          }
+        />
+        <span
+          className={
+            chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0 ? "right" : ""
+          }
+          >
+            {chatMessage.loggedInEmployee.firstName} {chatMessage.loggedInEmployee.lastName}
+        </span>
+        <p className={chatMessage.font}>{chatMessage.text}</p>
+        <span
+          className={
+            chatMessage.messageIndex && chatMessage.messageIndex % 2 === 0
+              ? "time-left"
+              : "time-right"
+          }>
+          {moment(chatMessage.date).format("h:mm a")}
+        </span>
+      </div>
     );
 }
 
