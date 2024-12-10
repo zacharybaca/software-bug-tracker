023e3ae35b55ff9d7ba2dbd18f547cd19cbf301e
zacharybaca <zachary.baca@msn.com>
2024-12-09 19:43:48 -0600

    started on media queries

--- client/software-bug-tracker/src/components/LandingPage/LandingPage.jsx
+++ client/software-bug-tracker/src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,7 @@
-import './landing-page.css';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { EmployeesContext } from '../../context/employeesContext';
-
+import "./landing-page.css";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { EmployeesContext } from "../../context/employeesContext";
 
 function LandingPage() {
   const context = React.useContext(EmployeesContext);
@@ -11,58 +10,89 @@ function LandingPage() {
     userID: "",
     password: "",
     avatarPic:
-      "https://api.dicebear.com/9.x/icons/svg?icon=search&backgroundType=gradientLinear&backgroundColor=eab581&size=130&radius=50",
+      "",
   };
 
   const [formData, setFormData] = React.useState(initialValues);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     context.login(formData);
+  };
+
+  function updateImageSrc() {
+    const screenWidth = window.innerWidth;
+    const size = screenWidth <= 768 ? 20 : screenWidth <= 1200 ? 70 : 130;
+    setFormData((prevState) => ({
+      ...prevState,
+      avatarPic: `https://api.dicebear.com/9.x/icons/svg?icon=search&backgroundType=gradientLinear&backgroundColor=eab581&size=${size}&radius=50`,
+    }));
   }
 
-    return (
-      <div id="landing-page-container">
-        <h1 id="form-header">Sign On To Access Your Assigned Tasks</h1>
-        <form id="login-form" name="loginForm" onSubmit={handleSubmit}>
-          <div id="landing-page-avatar">
-            <img src={formData.avatarPic ? formData.avatarPic : "/uploads/landing-page-avatar.png"} alt="landing page avatar" />
-          </div>
-          <input
-            type="text"
-            id="login-user-name"
-            name="userID"
-            value={formData.userID}
-            onChange={handleChange}
-            required
-            autoFocus
-            placeholder="Enter Username"
+  React.useEffect(() => {
+    updateImageSrc();
+    const handleResize = () => updateImageSrc();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div id="landing-page-container">
+      <h1 id="form-header">Sign On To Access Your Assigned Tasks</h1>
+      
+      <form id="login-form" name="loginForm" onSubmit={handleSubmit}>
+        <div id="landing-page-avatar">
+          <img
+            id="avatarImage"
+            src={formData.avatarPic}
+            
+            alt="landing page avatar"
           />
-          <input
-            type="password"
-            id="login-user-password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            placeholder="Enter Password"
-          />
-          <button className="btn btn-layered-3d btn-layered-3d--green">Sign On</button>
-          {context.userState.errMsg ? <p className="error-message">{context.userState.errMsg}</p> : ""}
-        </form>
-          <button type="button" id="new-user-button" onClick={() => navigate("/sign-up")}>
-            New User? Click Here
-          </button>
-      </div>
-    );
+        </div>
+        <input
+          type="text"
+          id="login-user-name"
+          name="userID"
+          value={formData.userID}
+          onChange={handleChange}
+          required
+          autoFocus
+          placeholder="Enter Username"
+        />
+        <input
+          type="password"
+          id="login-user-password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+          placeholder="Enter Password"
+        />
+        <button className="btn btn-layered-3d btn-layered-3d--green">
+          Sign On
+        </button>
+        {context.userState.errMsg && (
+          <p className="error-message">{context.userState.errMsg}</p>
+        )}
+      </form>
+      
+      
+      <button
+        type="button"
+        id="new-user-button"
+        onClick={() => navigate("/sign-up")}>
+        New User? Click Here
+      </button>
+    </div>
+  );
 }
 
 export default LandingPage;
