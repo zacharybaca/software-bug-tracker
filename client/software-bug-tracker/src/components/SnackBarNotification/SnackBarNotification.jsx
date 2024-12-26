8eeea26967eedec4e8d69dcb009d9a51c07d4d16
Zachary Baca <zachary.baca@msn.com>
2024-12-26 14:09:43 -0600

    Update SnackBarNotification.jsx

--- client/software-bug-tracker/src/components/SnackBarNotification/SnackBarNotification.jsx
+++ client/software-bug-tracker/src/components/SnackBarNotification/SnackBarNotification.jsx
@@ -1,11 +1,23 @@
 import './snack-bar-notification.css';
+import React, { useEffect } from "react";
 import chatEnteredImage from "../../assets/chat-entered.png";
 
 
-const SnackBarNotification = ({ message, type, showToast }) => {
+const SnackBarNotification = ({ message, type, showToast, onClose, duration = 3000 }) => {
+    const [visible, setVisible] = React.useState(true);
+
+    useEffect(() => {
+        setVisible(true);
+
+        const timer = setTimeout(() => {
+            onClose();
+        }, duration);
+        return () => clearTimeout(timer);
+    }, [onClose, duration]);
 
     return (
-        <div id="snack-bar-notification-container" className={showToast ? `atma-light ${type}-toast` : 'atma-light'}>
+        
+        <div id="snack-bar-notification-container" className={`toast-notification ${visible ? "show" : ""} atma-light ${type}-toast`}>
             <img src={chatEnteredImage} />
             <h1 id="snack-bar-notification-header">{message}</h1>
         </div>
