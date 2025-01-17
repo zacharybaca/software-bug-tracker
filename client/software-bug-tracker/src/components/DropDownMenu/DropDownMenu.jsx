98d89c1b5bf6e558d475e66066f3f6f5f1945afe
Zachary Baca <zachary.baca@msn.com>
2025-01-17 12:27:12 -0600

    Update DropDownMenu.jsx

--- client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
+++ client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import './drop-down-menu.css';
 import MenuIcon from '../../assets/menu-button.png';
 
 
 const DropDownMenu = () => {
+    const [showMenu, setShowMenu] = React.useState(false);
+
+    const toggleMenu = () => {
+        showMenu ? setShowMenu(false) : setShowMenu(true);
+    }
 
     return (
-        <div id="drop-down-container">
-            <img src={MenuIcon} id="menu-icon" alt="menu icon" />
+        <div id="drop-menu-container" toggleMenu={toggleMenu}>
+            {showMenu ? <div>Menu</div> :  <div id="drop-down-container">
+                <img src={MenuIcon} id="menu-icon" alt="menu icon" />
+            </div>}
         </div>
     )
 }
