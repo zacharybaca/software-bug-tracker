6a663ae122d4043bfa9da1ba79987005d243eeea
Zachary Baca <zachary.baca@msn.com>
2025-01-17 09:57:21 -0600

    Create DropDownMenu.jsx

---
+++ client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
@@ -0,0 +1,11 @@
+import './drop-down-menu.css';
+
+
+const DropDownMenu = () => {
+
+    return (
+        <div id="drop-down-container">drop down menu</div>
+    )
+}
+
+export default DropDownMenu;
