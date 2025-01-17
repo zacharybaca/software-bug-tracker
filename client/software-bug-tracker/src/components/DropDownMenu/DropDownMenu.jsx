b8765a3d700b2e78b7fdc5c5a6baac7859e6cb17
Zachary Baca <zachary.baca@msn.com>
2025-01-17 13:10:34 -0600

    Update DropDownMenu.jsx

--- client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
+++ client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './drop-down-menu.css';
 import MenuIcon from '../../assets/menu-button.png';
+import CloseButtonIcon from '../../assets/close-button.png';
 
 const DropDownMenu = () => {
     const [showMenu, setShowMenu] = React.useState(false);
+    const [showButton, setShowButton] = React.useState(false);
 
     const toggleMenu = () => setShowMenu(!showMenu);
+    const toggleButton = () => setShowButton(!showButton);
 
     return (
         <div id="drop-menu-container">
@@ -14,7 +17,13 @@ const DropDownMenu = () => {
             </button>
             {showMenu && (
                 <div id="drop-down-container">
-                    <div>Menu</div>
+                    {showButton && (
+                        <div id="drop-down-items-container">
+                            <div id="drop-down-items">
+                                <img src={CloseButtonIcon} id="close-button-icon" alt="close button icon" onClick={toggleButton}/>
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
