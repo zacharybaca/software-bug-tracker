f93e47a18710e15e416da76ce6ac16e1da6a5910
Zachary Baca <zachary.baca@msn.com>
2025-01-17 10:53:00 -0600

    Update DropDownMenu.jsx

--- client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
+++ client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,10 +1,13 @@
 import './drop-down-menu.css';
+import MenuIcon from '../../assets/menu-button.png';
 
 
 const DropDownMenu = () => {
 
     return (
-        <div id="drop-down-container">drop down menu</div>
+        <div id="drop-down-container">
+            <img src={MenuIcon} id="menu-icon" alt="menu icon" />
+        </div>
     )
 }
 
