c7f1db876672e82fed27b3a4b2b9f16fa22a39dd
Zachary Baca <zachary.baca@msn.com>
2025-01-24 10:46:40 -0600

    Update confirmationDialogBoxContext.jsx

--- client/software-bug-tracker/src/context/confirmationDialogBoxContext.jsx
+++ client/software-bug-tracker/src/context/confirmationDialogBoxContext.jsx
@@ -5,32 +5,34 @@ const ConfirmationDialogBoxContext = React.createContext();
 
 function ConfirmationDialogBoxContextProvider(props) {
     const [showDialog, setShowDialog] = React.useState(true);
+    const [proceed, setProceed] = React.useState(false);
     const [dialogQuestion, setDialogQuestion] = React.useState("");
 
     const handleConfirm = (e) => {
         if (e.target.value === 'confirm') {
             setShowDialog(false);
-            return true;
-        }
+            setProceed(true);
+        };
     };
 
     const handleCancel = (e) => {
         if (e.target.value === 'cancel') {
             setShowDialog(false);
-            return false;
-        }
+            setProceed(false);
+        };
     };
 
     const handleDialogQuestion = (question) => {
         setShowDialog(true);
         setDialogQuestion(question);
     };
-    
+
     return (
         <ConfirmationDialogBoxContext.Provider
             value={{
                 showDialog,
                 dialogQuestion,
+                proceed,
                 handleConfirm,
                 handleCancel,
                 handleDialogQuestion
