a29dd72bcea0475843c0ad04fc7716d9de0a33f2
Zachary Baca <zachary.baca@msn.com>
2025-01-29 10:09:18 -0600

    Create PasswordReset.jsx

---
+++ client/software-bug-tracker/src/components/PasswordReset/PasswordReset.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './password-reset.css';
+
+const PasswordReset = () => {
+    return (
+        <div>
+            <h1>Password Reset</h1>
+        </div>
+    )
+}
+
+export default PasswordReset;
