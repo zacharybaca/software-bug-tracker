600dd8e3b216e26e47dce11b8a6e5783e528bf0d
Zachary Baca <zachary.baca@msn.com>
2025-01-29 11:19:51 -0600

    Update PasswordReset.jsx

--- client/software-bug-tracker/src/components/PasswordReset/PasswordReset.jsx
+++ client/software-bug-tracker/src/components/PasswordReset/PasswordReset.jsx
@@ -3,8 +3,16 @@ import './password-reset.css';
 
 const PasswordReset = () => {
     return (
-        <div>
+        <div id="password-reset-container">
             <h1>Password Reset</h1>
+            <form id="password-reset-form">
+                <label htmlFor="access-code">Enter Access Code:</label>
+                <input type="number" id="access-code" name="access-code" />
+                <label htmlFor="new-password">Enter New Password:</label>
+                <input type="password" id="new-password" name="new-password" />
+                <label htmlFor="confirm-password">Confirm New Password:</label>
+                <input type="password" id="confirm-password" name="confirm-password" />
+            </form>
         </div>
     )
 }
