a885c1af3e1ffe46c397863881d64975f19851f4
Zachary Baca <zachary.baca@msn.com>
2025-01-29 12:44:19 -0600

    Update PasswordReset.jsx

--- client/software-bug-tracker/src/components/PasswordReset/PasswordReset.jsx
+++ client/software-bug-tracker/src/components/PasswordReset/PasswordReset.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import './password-reset.css';
 
 const PasswordReset = () => {
+    const [showPasswordForm, setShowPasswordForm] = React.useState(true);
+
+    const toggleShowPassword = () => {
+        setShowPasswordForm(!showPasswordForm);
+    };
+    
     return (
-        <div id="password-reset-container">
-            <h1>Password Reset</h1>
-            <form id="password-reset-form">
-                <label htmlFor="access-code">Enter Access Code:</label>
-                    <input 
-                        type="number" 
-                        id="access-code" 
-                        name="access-code" 
-                    />
-                <label htmlFor="new-password">Enter New Password:</label>
-                    <input 
-                        type="password" 
-                        id="new-password" 
-                        name="new-password" 
-                    />
-                <label htmlFor="confirm-password">Confirm New Password:</label>
-                    <input 
-                        type="password" 
-                        id="confirm-password" 
-                        name="confirm-password" 
-                    />
-            </form>
+        <div className="dialog-overlay">
+            <div className="dialog-content">
+                <div id="password-reset-container">
+                    <h1>Password Reset</h1>
+                    <form id="password-reset-form">
+                        <label htmlFor="access-code">Enter Access Code:</label>
+                            <input 
+                                type="number" 
+                                id="access-code" 
+                                name="access-code"
+                                placeholder="Enter Access Code" 
+                            />
+                        <label htmlFor="new-password">Enter New Password:</label>
+                            <input 
+                                type="password" 
+                                id="new-password" 
+                                name="new-password"
+                                placeholder="Enter New Password" 
+                            />
+                        <label htmlFor="confirm-password">Confirm New Password:</label>
+                            <input 
+                                type="password" 
+                                id="confirm-password" 
+                                name="confirm-password"
+                                placeholder="Confirm New Password" 
+                            />
+                        <button type="submit" className="glow-on-hover">Change Password</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
