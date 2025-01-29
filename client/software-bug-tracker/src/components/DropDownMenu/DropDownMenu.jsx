04eae08ec8b03df34dc843872793f13cba354fcd
Zachary Baca <zachary.baca@msn.com>
2025-01-29 09:10:52 -0600

    Update DropDownMenu.jsx

--- client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
+++ client/software-bug-tracker/src/components/DropDownMenu/DropDownMenu.jsx
@@ -3,14 +3,18 @@ import './drop-down-menu.css';
 import StickyNavBar from '../StickyNavBar/StickyNavBar';
 import MenuIcon from '../../assets/bug-menu-icon.png';
 import CloseButtonIcon from '../../assets/close-button.png';
+import { EmployeesContext } from '../../context/employeesContext';
 
 const DropDownMenu = (props) => {
     const [showMenu, setShowMenu] = React.useState(false);
+    const context = React.useContext(EmployeesContext);
+    const loggedInEmployee = context.getLoggedInEmployee();
 
     const toggleMenu = () => setShowMenu(!showMenu);
 
     return (
-        <div>
+        loggedInEmployee && (
+            <div>
             <button id="menu-toggle-button" className="glow-on-enter" onClick={toggleMenu}>
                 <img
                     src={showMenu ? CloseButtonIcon : MenuIcon}
@@ -24,6 +28,7 @@ const DropDownMenu = (props) => {
                 <StickyNavBar navigate={props.navigate} showMenu={showMenu} toggleMenu={toggleMenu} />
             </div>
         </div>
+        )
     );
 };
 
