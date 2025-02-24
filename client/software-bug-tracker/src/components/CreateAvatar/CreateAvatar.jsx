8f36426ddd89b500a644ed914d0b6ef5a1bc65d6
zacharybaca <zachary.baca@msn.com>
2025-02-23 20:28:20 -0600

    changed footer ui

--- client/software-bug-tracker/src/components/CreateAvatar/CreateAvatar.jsx
+++ client/software-bug-tracker/src/components/CreateAvatar/CreateAvatar.jsx
@@ -1,22 +1,21 @@
 import './create-avatar.css';
 import { createAvatar } from '@dicebear/core';
-import { funEmoji } from '@dicebear/collection';
+import { icons } from '@dicebear/collection';
 import { useMemo } from 'react';
 
 
 function CreateAvatar({ size }) {
     const randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
     const avatar = useMemo(() => {
-        return createAvatar(funEmoji, {
-        size: size,
-        seed: randomNumber,
-        radius: 50,
-        backgroundColor: ['b6e3f4', 'c0aede', 'd1d4f9', 'ffd5dc', 'ffdfbf'],
-        backgroundType: ['gradientLinear'],
-        eyes: ['closed', 'cute', 'glasses', 'pissed', 'plain', 'shades', 'stars', 'wink'],
-        mouth: ['cute', 'faceMask', 'lilSmile', 'pissed', 'plain', 'smileLol', 'smileTeeth', 'tongueOut', 'wideSmile']
+        return createAvatar(icons, {
+            size: size,
+            seed: randomNumber,
+            radius: 50,
+            backgroundColor: ['b6e3f4', 'c0aede', 'd1d4f9', 'ffd5dc', 'ffdfbf'],
+            backgroundType: ['gradientLinear'],
+            icon: ['bug', 'display', 'envelope', 'globe', 'globe2', 'inbox', 'inboxes', 'keyboard', 'laptop', 'mouse', 'mouse2', 'printer', 'router', 'search', 'webcam']
         }).toDataUri();
-    }, []);
+    }, [randomNumber, size]);
 
     return <img src={avatar} alt="Avatar" />;
 }
--- client/software-bug-tracker/src/components/Footer/Footer.jsx
+++ client/software-bug-tracker/src/components/Footer/Footer.jsx
@@ -4,9 +4,18 @@ import Logo from '../../assets/issue-insight-logo.png';
 function Footer() {
     return (
         <footer id="footer">
-            <p id="icon-para">Icons Provided By: <a href="https://icons8.com">Icons8</a></p>
-            <img src={Logo} alt="logo" />
-            <p id="copyright-para">©️ 2024</p>
+            <div id="app-info-container-footer">
+                <p id="icon-para">Developed & Designed by <a href="https://github.com/zacharybaca" target="_blank" rel="noopener noreferrer">Zachary</a></p>
+                <p>Built with React, Node.js, and MongoDB</p>
+            </div>
+            <div id="tagline-container-footer">
+                <h1 id="tagline">Issue Insight — Streamline bug tracking and team collaboration</h1>
+                <img src={Logo} alt="logo" />
+            </div>
+
+            <div id="tech-specs-container-footer">
+                <p id="copyright-para">©️ {new Date().getFullYear()} Issue Insight. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
