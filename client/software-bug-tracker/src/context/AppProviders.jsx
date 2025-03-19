e1c54cb065baded0a51285a96543b6cb15a08b87
Zachary Baca <zachary.baca@msn.com>
2025-03-19 08:31:35 -0500

    Update AppProviders.jsx

--- client/software-bug-tracker/src/context/AppProviders.jsx
+++ client/software-bug-tracker/src/context/AppProviders.jsx
@@ -4,19 +4,22 @@ import { TasksContextProvider } from "./tasksContext.jsx";
 import { SnackBarNotificationContextProvider } from "./snackBarNotificationContext.jsx";
 import { ConfirmationDialogBoxContextProvider } from "./confirmationDialogBoxContext";
 import { PasswordResetContextProvider } from "./passwordResetContext.jsx";
+import { ChatBotContextProvider } from "./chatBotContext.jsx";
 
 export const AppProviders = ({ children }) => {
   return (
     <Router>
-      <ConfirmationDialogBoxContextProvider>
-        <PasswordResetContextProvider>
-          <SnackBarNotificationContextProvider>
-            <EmployeesContextProvider>
-              <TasksContextProvider>{children}</TasksContextProvider>
-            </EmployeesContextProvider>
-          </SnackBarNotificationContextProvider>
-        </PasswordResetContextProvider>
-      </ConfirmationDialogBoxContextProvider>
+      <ChatBotContextProvider>
+        <ConfirmationDialogBoxContextProvider>
+          <PasswordResetContextProvider>
+            <SnackBarNotificationContextProvider>
+              <EmployeesContextProvider>
+                <TasksContextProvider>{children}</TasksContextProvider>
+              </EmployeesContextProvider>
+            </SnackBarNotificationContextProvider>
+          </PasswordResetContextProvider>
+        </ConfirmationDialogBoxContextProvider>
+      </ChatBotContextProvider>
     </Router>
   );
 };
