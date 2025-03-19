af175bf5a2c412821b8fc24ff244fb30e4e1a4bb
Zachary Baca <zachary.baca@msn.com>
2025-03-19 13:05:50 -0500

    Update ChatBot.jsx

--- client/software-bug-tracker/src/components/ChatBot/ChatBot.jsx
+++ client/software-bug-tracker/src/components/ChatBot/ChatBot.jsx
@@ -12,16 +12,16 @@ const ChatBot = () => {
                 <h1 onClick={chatBot.toggleChatBox}>Click Here for Help!</h1>
             </div> : 
             <div id="chatbot-box">
-                <button type="button" id="chat-box-close-button" className="glow-on-access" onClick={chatBot.toggleChatBox}>❎Close</button>
+                <div id="chatbox-heading">
+                    <h1>Issue Insight Chatbot</h1>
+                    <button type="button" id="chat-box-close-button" className="glow-on-access" onClick={chatBot.toggleChatBox}>❎Close</button>
+                </div>
                 <div id="chat--bot">
-                    <h1>Chatbot</h1>
-                    <h1>Chatbot2</h1>
-                    <h1>Chatbot3</h1>
-                    <h1>Chatbot4</h1>
-                    <h1>Chatbot5</h1>
-                    <h1>Chatbot6</h1>
-                    <h1>Chatbot7</h1>
-                    <h1>Chatbot8</h1>
+                    {<ul>
+                    {[...Array(15)].map((_, i) => (
+                        <li key={i}>Chatbot {i + 1}</li>
+                    ))}
+                    </ul>}
                 </div>
             </div>}
         </>
