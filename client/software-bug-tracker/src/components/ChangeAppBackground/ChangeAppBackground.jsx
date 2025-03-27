f8d738887ee53b6c7eebd765f2e49ccd1a46cac2
Zachary Baca <zachary.baca@msn.com>
2025-03-27 11:16:04 -0500

    Update ChangeAppBackground.jsx

--- client/software-bug-tracker/src/components/ChangeAppBackground/ChangeAppBackground.jsx
+++ client/software-bug-tracker/src/components/ChangeAppBackground/ChangeAppBackground.jsx
@@ -11,29 +11,38 @@ const ChangeAppBackground = () => {
         context.enableBackgroundOptions ? (
             <div className={context.enableBackgroundOptions ? "dialog-overlay" : "dialog-overlay-only-button-container"}>
                 <div className="dialog-items">
-                    <h2 className="background-question">
-                        {context.dialogQuestion ? context.dialogQuestion : ""}
-                    </h2>
-                    <div className="background-options">
-                        <label htmlFor="background">Select A Background</label>
-                        <select
-                            id="background"
-                            name="background"
-                            onChange={context.handleBackgroundOptions}
-                            value={context.background}
-                        >
-                            <option disabled>Select A Background</option>
-                            <option value="background1">Background 1</option>
-                            <option value="background2">Background 2</option>
-                            <option value="background3">Background 3</option>
-                            <option value="background4">Background 4</option>
-                            <option value="background5">Background 5</option>
-                            <option value="background6">Background 6</option>
-                        </select>
+                    <div id="background-selection-container">
+                        <div id="main-form-container">
+                            <div id="background-question-container">
+                                <h2 className="background-question">
+                                    Customize Wallpaper of Background
+                                </h2>
+                            </div>
+                            <br />
+                            <br />
+                            <hr />
+                            <div className="background-options">
+                                <label htmlFor="background">Select A Background</label>
+                                <select
+                                    id="background"
+                                    name="background"
+                                    onChange={context.handleBackgroundOptions}
+                                    value={context.background}
+                                >
+                                    <option disabled>Select A Background</option>
+                                    <option value="background1">Background 1</option>
+                                    <option value="background2">Background 2</option>
+                                    <option value="background3">Background 3</option>
+                                    <option value="background4">Background 4</option>
+                                    <option value="background5">Background 5</option>
+                                    <option value="background6">Background 6</option>
+                                </select>
+                            </div>
+                            <button type="button" className="close-button glow-on-arrival" onClick={() => context.toggleBackgroundOptions("Make a Background Selection for the App.")}>
+                                ❎ Close
+                            </button>
+                        </div>
                     </div>
-                    <button type="button" className="close-button glow-on-arrival" onClick={() => context.toggleBackgroundOptions("Make a Background Selection for the App.")}>
-                        ❎ Close
-                    </button>
                 </div>
             </div>
         )
@@ -42,7 +51,7 @@ const ChangeAppBackground = () => {
                 <ReusableStyledButton
                     type="button"
                     title="Set Wallpaper"
-                    clickFunction={() => context.toggleBackgroundOptions("Make a Background Selection for the App.")}
+                    clickFunction={() => context.toggleBackgroundOptions("Customize The Background of the Application")}
                 />
             </div>
     )
