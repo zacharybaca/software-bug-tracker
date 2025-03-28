1efff366a4df6eb9087fc00608e8ba698ba48ebd
Zachary Baca <zachary.baca@msn.com>
2025-03-28 12:01:17 -0500

    Update AppProviders.jsx

--- client/software-bug-tracker/src/context/AppProviders.jsx
+++ client/software-bug-tracker/src/context/AppProviders.jsx
@@ -1,23 +1,17 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { EmployeesContextProvider } from "./employeesContext.jsx";
-import { TasksContextProvider } from "./tasksContext.jsx";
-import { SnackBarNotificationContextProvider } from "./snackBarNotificationContext.jsx";
-import { ConfirmationDialogBoxContextProvider } from "./confirmationDialogBoxContext";
-import { PasswordResetContextProvider } from "./passwordResetContext.jsx";
-import { ChatBotContextProvider } from "./chatBotContext.jsx";
-
 export const AppProviders = ({ children }) => {
   return (
     <Router>
       <ChatBotContextProvider>
         <ConfirmationDialogBoxContextProvider>
-            <SnackBarNotificationContextProvider>
-              <EmployeesContextProvider>
-                <PasswordResetContextProvider>
-                  <TasksContextProvider>{children}</TasksContextProvider>
-                </PasswordResetContextProvider>
-              </EmployeesContextProvider>
-            </SnackBarNotificationContextProvider>
+          <SnackBarNotificationContextProvider>
+            <EmployeesContextProvider>
+              <PasswordResetContextProvider>
+                <TasksContextProvider>
+                  {children}
+                </TasksContextProvider>
+              </PasswordResetContextProvider>
+            </EmployeesContextProvider>
+          </SnackBarNotificationContextProvider>
         </ConfirmationDialogBoxContextProvider>
       </ChatBotContextProvider>
     </Router>
