c20f04878a88fd0d2d9013112c671b54de362d31
Zachary Baca <zachary.baca@msn.com>
2025-03-28 07:41:43 -0500

    Update passwordResetContext.jsx

--- client/software-bug-tracker/src/context/passwordResetContext.jsx
+++ client/software-bug-tracker/src/context/passwordResetContext.jsx
@@ -26,28 +26,34 @@ function PasswordResetContextProvider({ children }) {
     const handleShowPasswordResetForm = () => setShowPasswordResetForm(true);
     const handleClosePasswordResetForm = () => setShowPasswordResetForm(false);
 
-    const updateEmployeePassword = async (userID, accessCode, newPassword, confirmPassword, employeeID) => {
+    const updateEmployeePassword = async (userID, accessCode, newPassword, confirmPassword) => {
         try {
+            const employee = employeesContext.employees.find(emp => emp.user.userID === userID);
+    
+            if (!employee) {
+                throw new Error("User not found");
+            }
+    
             const updatedInfo = { userID, accessCode, newPassword, confirmPassword };
-
-            const response = await fetch(`/api/employees/${employeeID}/reset-password`, {
+    
+            const response = await fetch(`/api/employees/${employee._id}/reset-password`, {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(updatedInfo),
             });
-
+    
             if (!response.ok) throw new Error(`Failed to update password: ${response.statusText}`);
-
+    
             const updatedEmployee = await response.json();
-
+    
             employeesContext.setEmployees(prevEmployees =>
-                prevEmployees.map(employee => (employee._id !== employeeID ? employee : updatedEmployee))
+                prevEmployees.map(emp => (emp._id !== employee._id ? emp : updatedEmployee))
             );
-
+    
         } catch (error) {
             employeesContext.handleAuthErr(error.message);
         }
-    };
+    };    
 
     return (
         <PasswordResetContext.Provider
